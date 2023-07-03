10771f91fae6fdcd6a881a60bdcb777e84827bcf
Ahmad <azafar506@gmail.com>
2023-07-03 11:00:38 +0500

    a commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Welcome to jazzer");  
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("C:\\Users\\fujitsu\\Documents\\best.pt")
+model.predict(source="0", show=True, conf=0.5)
