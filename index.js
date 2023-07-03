13adddb5c5035eb81f6ba5c05aebbb51dadfe13b
Ahmad <azafar506@gmail.com>
2023-07-03 11:15:53 +0500

    commiting to master

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-import cv2
-
-
-from ultralytics import YOLO
-import cv2
-
-model = YOLO("C:\\Users\\fujitsu\\Documents\\best.pt")
-model.predict(source="0", show=True, conf=0.5)
+console.log("Welcome, to JAZZER");
